2c2041cb1001cfe5cea03015ce7c25c503bbca6a
gitadityakumar <itsadityasingh05@gmail.com>
2025-04-21 23:17:20 +0530

    working change

--- constants/data.ts
+++ constants/data.ts
@@ -2,9 +2,22 @@ export const STREAMS = {
   india: [
     'https://ndtv24x7elemarchana.akamaized.net/hls/live/2003678-b/ndtv24x7/master.m3u8',
     'https://dwby15d04agvq.cloudfront.net/index_5.m3u8',
+    'https://nw18live.cdn.jio.com/bpk-tv/CNBC_TV18_NW18_MOB/output01/CNBC_TV18_NW18_MOB-audio_98834_eng=98800-video=3724000.m3u8',
+    'https://nw18live.cdn.jio.com/bpk-tv/CNN_News18_NW18_MOB/output01/CNN_News18_NW18_MOB-audio_98835_hin=98800-video=3724000.m3u8',
+    'https://dztlhgid9me95.cloudfront.net/live-tv/Vidgyor/timesnow/live_720p/chunks.m3u8',
+
   ],
   usa: [
     'https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/694940094001/a49b3803-ef54-4746-a98f-744840d6f5a1/10s/master.m3u8?fastly_token=NjhlOGEzZDNfOWNlM2I5YzBhYTgzMTlhMzE1MTViZmRmMWNkOWU3NGJiMTgwZmM5MGRhNjc1ZDU4ZTkxOWM2MjE0MDk1ZGM3OQ%3D%3D',
-    'https://dai.google.com/linear/hls/pb/event/BVlOmFGaTi6NpchccR14yA/stream/83399e4e-70c5-4755-b93b-f807fe18ee54:TPE/master.m3u8',
+    'https://dai.google.com/linear/hls/pb/event/BVlOmFGaTi6NpchccR14yA/stream/e7473468-4602-4a26-839c-ccd71f7b4bdd:TPE2/master.m3u8',
+    'https://d1ewctnvcwvvvu.cloudfront.net/v1/master/7b67fbda7ab859400a821e9aa0deda20ab7ca3d2/yahooLive/playlist.m3u8?ads.D_ID=%5BD_ID%5D&ads.HS_URL=http%3A%2F%2Fhaystack.tv%2Fid%2F45AsUAVdb&ads.HS_LIVE_AD_TOKEN=3d5ae581763be2fce3d8fb7a1c43eda55d7f87039d086b6c27f90c8fea6194c2d1fee74ceddae92efc18f4cdad9eb75ffe82dbf788f0c5b8045218f89c5153b8&ads.us_privacy=1---&ads.D_MK=web&ads.D_ML=%5BSS_D_ML%5D&ads.D_OV=4.6&ads.D_DNT=%5BD_DNT%5D',
+    'https://livenewsplayer.com:1936/cnnsd/cnnsd//playlist.m3u8?dvr&secendtime=1745257480&sechash=mDX9mYnauuv0nbspGU3bBnQYOnMiwb5khpRcQbijK54=&secstarttime=1745246680',
+    'https://nw18live.cdn.jio.com/bpk-tv/CNBC_TV18_NW18_MOB/output01/index.m3u8?__hdnea__=st=1744949212~exp=1745035612~acl=/*~hmac=87efaf4076bb149a5eee7a113b35034e2aad785c768264076d9cfb6fb43021f2'
+
+
   ],
 };
+
+
+//zee business, cnbc zwaz , bloomberg usa , cnn business, cnbc, timesofindia
+//, bloomberg, foxNews, cnbc, Yahoo Finance Live, CNN Live Streaming
