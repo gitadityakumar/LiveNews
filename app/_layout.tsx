e967451d91e1b4b925abb434eb06b57970666869
gitadityakumar <itsadityasingh05@gmail.com>
2026-02-03 01:14:39 +0530

    add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,9 @@ import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { SplashScreen } from 'expo-router';
 import { fullscreenEmitter } from './(tabs)/_layout';
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
   const [isFullscreen, setIsFullscreen] = useState(false);
   useFrameworkReady();
