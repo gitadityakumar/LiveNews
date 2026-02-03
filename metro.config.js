f0429d996bc3af005ff625f125f003d4e1a0cf2c
gitadityakumar <iamadityakumar05@proton.me>
2026-02-03 21:41:30 +0530

    app otimization

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+import { getDefaultConfig } from 'expo/metro-config';
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+    inlineRequires: true,
+  },
+});
+
+export default config;
