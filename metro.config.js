db70236a270b29fdcff979bf9388ea85606f28b0
gitadityakumar <iamadityakumar05@proton.me>
2026-02-03 22:30:11 +0530

    minor error fix

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,5 @@
-import { getDefaultConfig } from 'expo/metro-config';
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer.getTransformOptions = async () => ({
-  transform: {
-    experimentalImportSupport: true,
-    inlineRequires: true,
-  },
-});
-
-export default config;
+module.exports = config;
